642213c69423506663f5f145dac9837d0346f8ce
Matthias <matthias@joker-it.ch>
2022-07-26 13:52:49 +0200

    get Microsoft Graph permission Ids

---
+++ get-MicrosoftGraph-PermissionId.ps1
@@ -0,0 +1,27 @@
+﻿$url = "https://raw.githubusercontent.com/microsoftgraph/microsoft-graph-docs/main/concepts/includes/permissions-ids.md"
+$header = "|---------------------------------------------------------|-------------|--------------------------------------|" # find starting point of permissions list
+
+# get raw result from github page (base of Microsoft doc article)
+$result = Invoke-RestMethod -Uri $url
+
+# split multiline text to array
+$aResult = $result.Split([Environment]::NewLine)
+
+# get index of starting point in permission list (by specific header)
+# https://devblogs.microsoft.com/scripting/find-the-index-number-of-a-value-in-a-powershell-array/
+$indexHeader = ([array]::indexof($aResult,$header))
+
+$hPermissions = @()
+
+# add each permission to list
+ForEach ($line in $aResult | Select -Skip ($indexHeader+1)) {
+    $aPermission = $line.Split("|") # split in different columns
+    $a = New-Object -TypeName PSObject
+    $a | Add-Member -NotePropertyName Permission -NotePropertyValue $aPermission[1]
+    $a | Add-Member -NotePropertyName Scope -NotePropertyValue $aPermission[2]
+    $a | Add-Member -NotePropertyName Id -NotePropertyValue $aPermission[3]
+    $hPermissions += $a
+}
+
+# show permissions
+$hPermissions| Out-GridView

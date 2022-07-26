b4c7863a0c76cd11591d65c858a8fc0b1848474d
Matthias <matthias@joker-it.ch>
2022-07-26 14:11:16 +0200

    optimize Trim and add example to find id

--- get-MicrosoftGraph-PermissionId.ps1
+++ get-MicrosoftGraph-PermissionId.ps1
@@ -1,4 +1,4 @@
-﻿$url = "https://raw.githubusercontent.com/microsoftgraph/microsoft-graph-docs/main/concepts/includes/permissions-ids.md"
+$url = "https://raw.githubusercontent.com/microsoftgraph/microsoft-graph-docs/main/concepts/includes/permissions-ids.md"
 $header = "|---------------------------------------------------------|-------------|--------------------------------------|" # find starting point of permissions list
 
 # get raw result from github page (base of Microsoft doc article)
@@ -14,14 +14,19 @@ $indexHeader = ([array]::indexof($aResult,$header))
 $hPermissions = @()
 
 # add each permission to list
-ForEach ($line in $aResult | Select -Skip ($indexHeader+1)) {
+ForEach ($line in $aResult | Select -Skip ($indexHeader+1) | Select -SkipLast 1) {
     $aPermission = $line.Split("|") # split in different columns
     $a = New-Object -TypeName PSObject
-    $a | Add-Member -NotePropertyName Permission -NotePropertyValue $aPermission[1]
-    $a | Add-Member -NotePropertyName Scope -NotePropertyValue $aPermission[2]
-    $a | Add-Member -NotePropertyName Id -NotePropertyValue $aPermission[3]
+    $a | Add-Member -NotePropertyName Permission -NotePropertyValue $aPermission[1].Trim()
+    $a | Add-Member -NotePropertyName Scope -NotePropertyValue $aPermission[2].Trim()
+    $a | Add-Member -NotePropertyName Id -NotePropertyValue $aPermission[3].Trim()
     $hPermissions += $a
 }
 
 # show permissions
 $hPermissions| Out-GridView
+
+#example to find specfic id
+$search = "AccessReview.Read.All"
+$id = ($hPermissions| Where {$_.Scope -eq "Application" -and $_.Permission -eq $search}).Id
+
